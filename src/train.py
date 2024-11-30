e22eef71bbbeb88df3956873d0bbd0f00a73b197
sda-test1 <sda_test@onet.pl>
2024-11-30 22:58:17 +0000

    Add mlflow

--- src/train.py
+++ src/train.py
@@ -1,18 +1,27 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from joblib import dump
+import mlflow
+from sklearn.metrics import accuracy_score
 
+def train_model():
+  with mlflow.start_run():
+    df = pd.read_csv("data/otodom.csv")
+    print(len(df))
+    df = df[df['price'].notnull()]
+    X = df[['area']]
+    y = df['price']
 
-df = pd.read_csv("data/otodom.csv")
-print(len(df))
-df = df[df['price'].notnull()]
-X = df[['area']]
-y = df['price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    model = RandomForestClassifier(max_depth=5, n_estimators=5)
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    mlflow.log_metric("accuracy", accuracy)
 
-model = RandomForestRegressor(max_depth=5, n_estimators=5)
-model.fit(X_train, y_train)
+    dump(model, 'models/model.pkl')
 
-dump(model, 'models/model.pkl')
+if __name__ == '__main__':
+  train_model()
