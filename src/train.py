e811cf3b7d679e40ac3e64390f7b34f12e0d57c9
sda-test1 <sda_test@onet.pl>
2024-12-08 08:45:33 +0000

    Added integration with mlflow

--- src/train.py
+++ src/train.py
@@ -2,6 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from joblib import dump
+import mlflow
+
+REMOTE_MLFLOW_URI = 'https://785c-34-80-185-238.ngrok-free.app'
+mlflow.set_tracking_uri(REMOTE_MLFLOW_URI)
 
 df = pd.read_csv('data/otodom.csv')
 df = df.dropna()
@@ -18,4 +22,9 @@ model.fit(X_train, y_train)
 score = model.score(X_test, y_test)
 assert score > 0.7, 'Unsatisfied score'
 print('Model score:', score)
+
+mlflow.set_experiment('Krzysio')
+with mlflow.start_run():
+  mlflow.log_metric('r2', score)
+
 dump(model, 'models/model.joblib')
